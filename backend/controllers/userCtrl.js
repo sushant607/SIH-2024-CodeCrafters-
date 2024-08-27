1d981f4f5916c9b1d22da2769590fe4f089fe2a7
meggha <megha.wadher@gmail.com>
2024-08-27 15:48:39 +0530

    login register

---
+++ backend/controllers/userCtrl.js
@@ -0,0 +1,83 @@
+import bcrypt  from 'bcrypt';
+import  jwt from 'jsonwebtoken' ;
+import { user } from "../models/user.js";
+
+const loginController = async (req, res) => {
+    try {
+        // Find user by email
+        const existingUser = await user.findOne({ email: req.body.email });
+        if (!user) {
+          return res
+            .status(200)
+            .send({ message: "user not found", success: false });
+        }
+        const isMatch = await bcrypt.compare(req.body.password, existingUser.password);
+        if (!isMatch) {
+          return res
+            .status(200)
+            .send({ message: "Invalid EMail or Password", success: false });
+        }
+        const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET, { expiresIn: "1d",  });
+        res.status(200).json({
+            success: true,
+            message: 'Logged in successfully',
+            token
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error logging in' });
+    }
+};
+
+const registerController = async (req, res) => {
+    try {
+        const exisitingUser = await user.findOne({ email: req.body.email });
+        if (exisitingUser) {
+          return res
+            .status(200)
+            .send({ message: "User Already Exist", success: false });
+        }
+        // Hash the password
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        req.body.password = hashedPassword;
+       // Create a new user instance
+       const newUser = new user(req.body);
+       await newUser.save();
+        res.status(201).json({ 
+            success: true,
+            message: 'Signed up successfully'
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+const authController = async (req, res) => {
+    try {
+      const users = await user.findById({ _id: req.body.userId });
+      users.password = undefined;
+      if (!users) {
+        return res.status(200).send({
+          message: "user not found",
+          success: false,
+        });
+      } else {
+        res.status(200).send({
+          success: true,
+          data: users,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "auth error",
+        success: false,
+        error,
+      });
+    }
+  };
+
+export { loginController, registerController, authController };
