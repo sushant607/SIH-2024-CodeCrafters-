6bbc3120dec0a2e54521b23e002b63e169ab9a95
Gaurav Prashant Sawant <gpsgaurav012@gmail.com>
2024-08-28 09:43:49 +0530

    app and index styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,18 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupPage from "./components/Signup/SignupPage";
 import JobPage from './components/JobPage/JobPage'
+import Home from "./components/Home/Home"
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <> 
       <BrowserRouter>
+      <Navbar/>
       <Routes>
-        <Route path="/" element={<SignupPage />} />
+        <Route path='/' element={<Home/>} />
+        {/* <Route path="/" element={<SignupPage />} /> */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/job" element={<JobPage />} />
       </Routes>
