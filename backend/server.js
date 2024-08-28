ba1784750d411a07eb9dc3ea7bd4913ae75ec48d
sushant607 <svbagul_b22@ce.vjti.ac.in>
2024-08-28 10:29:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,20 @@ import colors from "colors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import {connectDB} from "./config/db.js";
-import { router as userRoutes } from "./routes/userRoutes.js";
+import { router, router as userRoutes } from "./routes/userRoutes.js";
 import cors from "cors";
 //dotenv conig
 dotenv.config();
 
 //connect to database
 connectDB();
-
 //rest object
 const app = express();
 app.use(cors())
 //middlewares
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(router);
 //routes
 app.use("/api/v1/user", userRoutes);
 
