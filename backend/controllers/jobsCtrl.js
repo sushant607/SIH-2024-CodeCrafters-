a791108cb7f7263de41b4b90dfebbd50a3cc9fa1
meggha <megha.wadher@gmail.com>
2024-08-29 23:48:35 +0530

    org

--- backend/controllers/jobsCtrl.js
+++ backend/controllers/jobsCtrl.js
@@ -1,6 +1,6 @@
-import  jwt from 'jsonwebtoken' ;
 import { user } from "../models/user.js";
 import { Job } from '../models/jobs.js';
+import { Organisation } from "../models/employee.js";
 
 // Create job
 const createJobController = async (req, res) => {
@@ -26,7 +26,6 @@ const getAllJobsController = async (req, res) => {
 // Get a single job by ID
 const getJobByIdController = async (req, res) => {
   try {
-    console.log("Route hit with ID:", req.params.id);
     const job = await Job.findById(req.params.id); 
     if (!job) {
       return res.status(404).json({ message: "Job not found" }); 
@@ -37,5 +36,71 @@ const getJobByIdController = async (req, res) => {
   }
 };
 
-export { createJobController, getAllJobsController, getJobByIdController };
+// Update job
+const updateJobController = async (req, res) => {
+  try {
+    const jobs = await Job.findOneAndUpdate(
+      { _id: req.params.id },  
+      req.body,          
+    );
+    res.status(201).send({
+      success: true,
+      message: "Job Profile Updated"
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Job Profile Update issue",
+      error,
+    });
+  }
+};
+
+// Delete job
+const deleteJobController = async (req, res) => {
+  try {
+    const job = await Job.findByIdAndDelete(req.params.id);
+    if (!job) {
+      return res.status(404).send({ message: "Job not found", success: false });
+    }
+    res.status(200).send({ message: "Job deleted successfully", success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Job Profile Delete issue",
+      error,
+    });
+  }
+};
+
+// View jobs company specific
+const viewJobsCompanySpecific = async (req, res) => {
+  try {
+    const organization = await Organisation.findById(req.params.companyId).populate('Jobs_offered');
+
+    if (!organization) {
+      return res.status(404).send({ message: "Company not found", success: false });
+    }
+
+    const jobs = await Job.find({ _id: { $in: organization.Jobs_offered } });
+
+    res.status(200).json({
+      success: true,
+      company: organization.Company_name,
+      jobs,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error retrieving jobs for the company', error });
+  }
+}
+
+// Applications
+
+
+
+
+export { createJobController, getAllJobsController, getJobByIdController, updateJobController, deleteJobController, viewJobsCompanySpecific };
 
---
+++ backend/routes/organisationRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {  createOrganisationController, getAllOrgController, getOrgByIdController, deleteOrgController, updateOrgController } from "../controllers/organisationCtrl.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+//routes
+//CREATE_Organisation || POST
+router.post("/create_org", authMiddleware, createOrganisationController);
+
+//VIEW_ALL_ORG || GET
+router.get("/get_all_org", authMiddleware, getAllOrgController);
+
+//VIEW_ORG || GET
+router.get("/get_id_org/:id", authMiddleware, getOrgByIdController);
+
+//UPDATE_JOB || POST
+router.post("/delete_id_org/:id", authMiddleware, deleteOrgController);
+
+//UPDATE_ORG || POST
+router.post("/update_id_org/:id", authMiddleware, updateOrgController);
+
+
+export { router };
