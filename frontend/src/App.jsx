50a363ae7d8247b1eac978c76b13346af40a14d8
Gaurav Prashant Sawant <gpsgaurav012@gmail.com>
2024-09-05 02:00:42 +0530

    reject-recruit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import JobPage from "./components/JobPage/JobPage";
 import ProfileSection from "./components/ViewProfile/Profile";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
 import OrgProfile from "./components/OrgProfile/OrgProfile";
 import UpdateOrgProfile from "./components/UpdateOrgProfile/UpdateOrgProfile";
 import JobList from "./components/JobList/JobList";
@@ -18,7 +18,8 @@ import OrgPageListing from "./components/OrgPageList/OrgPageListing";
 import JobandUser from "./components/JobandUser/JobandUser";
 import OrgProfileSection from "./components/ViewOrgProfile/ViewOrgProfile";
 import Features from "./components/Features/Features";
-import RecommendedJobs from './components/RecommendedJobs/RecommendedJobs'
+import RecommendedJobs from "./components/RecommendedJobs/RecommendedJobs";
+import ViewApplicants from "./components/ViewApplicants/ViewApplicants";
 function App() {
   return (
     <>
@@ -34,18 +35,18 @@ function App() {
           <Route path='/jobList' element={<JobList />} />
           <Route path='/orgJobList' element={<OrgPageListing />} />
           <Route path='/signup' element={<SignupPage />} />
-          <Route path='/createProfile' element={<CreateProfile/>} />
-          <Route path='/updateProfile' element={<UpdateProfile/>} />
+          <Route path='/createProfile' element={<CreateProfile />} />
+          <Route path='/updateProfile' element={<UpdateProfile />} />
           <Route path='/userProfile' element={<ProfileSection />} />
           <Route path='/viewOrgProfile' element={<OrgProfileSection />} />
           <Route path='/orgProfile' element={<OrgProfile />} />
           <Route path='/currentjobs' element={<JobPage />} />
           <Route path='/job/:id' element={<JobPage />} />
-          <Route path='/addjob' element={<AddJob/>} />
-          <Route path='/recommendedJobs' element={<RecommendedJobs/>} />
-
+          <Route path='/addjob' element={<AddJob />} />
+          <Route path='/recommendedJobs' element={<RecommendedJobs />} />
+          <Route path='/recruit/:id' element={<ViewApplicants />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </>
   );
---
+++ frontend/src/components/ViewApplicants/ApplicantCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState } from "react";
+import "./ApplicantCard.css";
+import dropdown from "../../../public/dropdown.png";
+const ApplicantCard = (props) => {
+  const [detail, setDetail] = useState(false);
+  return (
+    <div className={`card`}>
+      {detail ? (
+        <div className='text-left'>
+          <p className='name'>{props.name}</p>
+          <p className='email'>{props.email}</p>
+          <p className='email'>{props.skills}</p>
+          <p className='email'>{props.about}</p>
+          <div className="mt-6 flex gap-6">
+            <button class='recruit-button'>RECRUIT</button>
+            <button class='reject-button'>REJECT</button>
+          </div>
+        </div>
+      ) : (
+        <div className='text-left'>
+          <p className='name'>{props.name}</p>
+          <p className='email'>{props.email}</p>
+        </div>
+      )}
+      <div className='img-cont'>
+        <img
+          src={dropdown}
+          alt='dropdown'
+          onClick={() => {
+            setDetail(!detail);
+          }}
+          className={`dropdown-arrow ${detail ? "dropdown-arrow-clicked" : ""}`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ApplicantCard;
