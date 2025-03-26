01fa3d37920bf599a3616a227b9604e72f0f8da7
sushant607 <144811720+sushant607@users.noreply.github.com>
2025-03-26 20:09:33 +0530

    Update ViewOrgProfile.jsx

--- frontend/src/components/ViewOrgProfile/ViewOrgProfile.jsx
+++ frontend/src/components/ViewOrgProfile/ViewOrgProfile.jsx
@@ -12,7 +12,7 @@ const OrgProfileSection = () => {
     const fetchOrgData = async () => {
       try {
         const response = await axios.get(
-          "https://sih-2024-codecrafters-1.onrender.com",
+          "https://sih-2024-codecrafters-1.onrender.com/api/v1/org/view_profile",
           {
             headers: {
               Authorization: `Bearer ${localStorage.getItem("token")}`,
